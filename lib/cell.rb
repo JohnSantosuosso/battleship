d69e708cad71426b48cd473bebd2a8847f052a7b
Chris Lyman <christopher.lyman0@gmail.com>
2022-05-18 13:30:56 -0400

    Add fired_upon? and fire_upon methods

---
+++ lib/cell.rb
@@ -0,0 +1,32 @@
+class Cell
+attr_reader :coordinate,
+            :ship
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @ship = nil
+    @fired_upon = false
+  end
+
+  def empty?
+    if ship == nil
+      true
+    else
+      false
+    end
+  end
+
+  def place_ship(boat)
+    @ship = boat
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    if @ship != nil
+      @ship.hit
+      @fired_upon = true
+    end
+  end
+end
