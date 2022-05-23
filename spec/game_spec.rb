c3d3f54105cf6bbd606801e73bfed0eb5046b9a1
John Santosuosso <john.santosuosso1@gmail.com>
2022-05-23 12:11:44 -0400

    add initialize spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,23 @@
-require 'board'
-require 'cell'
-require 'ship'
-require 'messages'
+require './lib/game.rb'
+require './lib/board.rb'
+require './lib/cell.rb'
+require './lib/ship.rb'
+require './lib/messages.rb'
 
 RSpec.describe Game do
+  before do
+    @game = Game.new
+  end
+
+  describe '#initialize' do
+    it 'is an instance of game' do
+      expect(@game).to be_an_instance_of(Game)
+    end
+
+    it 'it has boards by default' do
+      expect(@game.player_board).to be_an_instance_of(Board)
+      expect(@game.computer_board).to be_an_instance_of(Board)
+    end
+
+  end
+end
